2f4464967057ca9b1e07a74ecd7bc6cf7f9e9af2
vishnu <vishnuchudhary2020@gmail.com>
2026-05-28 10:04:54 +0530

    add implementations for list element addition and sum of even indexed elements

---
+++ list/addElemntinList.py
@@ -0,0 +1,15 @@
+'''print("enter the size of list ")
+n=int(input("enter size "))
+arr=[]
+sum=0
+for i in range(n):
+    x=int(input("enter element "))
+    arr.append(x)
+print(arr)
+for i in arr:
+    sum+=i
+
+print("the sum of given list is ",sum)'''
+
+n=int(input("enter size "))
+
---
+++ list/sumofeven.py
@@ -0,0 +1,26 @@
+'''n=int(input("eneter size "))
+arr=[]
+sum=0
+for i in range(n):
+    x=int(input('enter elemt '))
+    arr.append(x)
+print(arr)
+for i in range(1,len(arr)+1):
+    if i%2==0:
+        sum+=arr[i]
+print(sum)'''
+
+
+n=int(input("enter size "))
+arr2=[]
+sum=0
+i=0
+while i<n:
+    x=int(input("enter elemt "))
+    arr2.append(x)
+    i+=1
+j=1
+while j<=len(arr2):
+    if j%2==0:
+        sum+=arr2[j]
+    j+=1
